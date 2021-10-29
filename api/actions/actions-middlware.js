99b03cf673f885e77829ec712bb5983d47a2943f
Sebastian <sebas6292@gmail.com>
2021-10-29 10:05:41 -0700

    created handleError middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,16 @@
 // add middlewares here related to actions
+const Action = require('./actions-model')
+// const yup = require('yup');
+
+function handleError(err, req, res, next) { //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message, 
+        prodMessage: 'something went terribly wrong!',
+    })
+}
+
+
+
+module.exports = {
+    handleError,
+}
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found`})
+});
 
 module.exports = server;
