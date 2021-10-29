3b751ed403dca909710837b31e8658a6c4a898db
Sebastian <sebas6292@gmail.com>
2021-10-29 10:26:46 -0700

    created a validationId middlware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,16 +1,34 @@
 // add middlewares here related to actions
 const Action = require('./actions-model')
-// const yup = require('yup');
 
 function handleError(err, req, res, next) { //eslint-disable-line
-    res.status(err.status || 500).json({
+        res.status(err.status || 500).json({
         message: err.message, 
-        prodMessage: 'something went terribly wrong!',
+        prodMessage: 'Something went terribly wrong!',
     })
+}
+
+async function validateActionId (req, res, next) { 
+    try { 
+        const action = await Action.get(req.params.id)
+        if (!action) { 
+            res.status(404).json({
+            message: 'Action is not found!',
+            })
+        } else {
+            req.action = action
+            next()
+        }
+    } catch (err) {
+        res.status(500).json({
+        message: 'Problem finding actions!',
+        })
+    }
 }
 
 
 
 module.exports = {
     handleError,
+    validateActionId
 }
